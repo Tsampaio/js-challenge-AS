802ebe91a4fd6abb098db111266a60fdba9a2c9c
Telmo Sampaio <telmoasampaio@gmail.com>
2020-11-02 23:42:00 +0000

    Adding routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import HomePage from './pages/HomePage';
+import PageNotFound from './components/PageNotFound';
+import RepositoryPage from './pages/RepositoryPage';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Test</h1>
-    </div>
+    <Router>
+      <main className="py-3">
+        <Container>
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/repository/:id" component={RepositoryPage} />
+            <Route path="*" component={PageNotFound} />
+          </Switch>
+        </Container>
+      </main>
+    </Router>
   );
 }
 
